b14ee99ff4b0ea2dc11866f64f7ac9055590a9fb
lishengzhi <18334905070@163.com>
2025-12-09 09:37:20 +0800

    feat: 添加注释

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 function deepCloneWithSpread(obj) {
+  // 如果是数组，递归拷贝每一项
   if (Array.isArray(obj)) return obj.map((item) => deepCloneWithSpread(item));
+  // 如果是对象（且不为 null），使用 Object.entries + reduce 递归拷贝每个属性
+  // 注意：函数类型、原始值（number/string/boolean/undefined/null）会原样返回
   if (typeof obj === "object" && obj !== null) {
     return Object.entries(obj).reduce((clone, [key, value]) => {
+      // 这里可以加日志用于调试：当前处理的键、值以及已构建的克隆对象
       console.log(key, value, clone);
       clone[key] = deepCloneWithSpread(value);
       return clone;
@@ -45,6 +49,7 @@ function deepCloneWithSpread(obj) {
 // console.log(clonedObj, "---");
 
 function* fn() {
+  // 生成器示例：每次调用 `next()` 依次返回下一个 yield 的值
   yield "a";
   yield "b";
   yield "c";
