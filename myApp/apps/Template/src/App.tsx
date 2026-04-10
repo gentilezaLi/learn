931b24fd5a75a8bba2f7a8a0fcadeadc980fcd85
lishengzhi <18334905070@163.com>
2026-04-10 12:29:13 +0800

    add: 通配符子模块

--- myApp/apps/Template/src/App.tsx
+++ myApp/apps/Template/src/App.tsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
 import '@/App.less';
 import { routes } from '@/pages/routes';
+import { add } from '@auto/utils';
 
 function AppRoutes() {
   const element = useRoutes(routes);
@@ -9,6 +10,12 @@ function AppRoutes() {
 }
 
 function App() {
+  useEffect(() => {
+    // 测试使用 @auto/utils 中的 add 方法
+    const result = add(1, 123);
+    console.log('utils add result:', result);
+  }, []);
+
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
--- myApp/apps/Template/vite.config.ts
+++ myApp/apps/Template/vite.config.ts
@@ -60,6 +60,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@auto': path.resolve(__dirname, '../../packages'),
     },
   },
 });
